9420212a99556973e77a610319cd9bbbeebbbc99
Ngina <Nginagitau11@outlook.com>
2022-04-24 19:48:11 +0300

    update: removes folders and adds proper test files

--- user.py
+++ user.py
@@ -5,7 +5,7 @@ class User:
     Class that instantiates the user and their details.
     '''
 
-    account_list =[]
+    user_list =[]
     def __init__(self, user_name, email, password):
         '''
         init method for the user details 
