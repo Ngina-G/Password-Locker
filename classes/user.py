2182e25373232dfb0d199388907ed98cee73618f
Ngina <Nginagitau11@outlook.com>
2022-04-24 19:34:23 +0300

    update: adds the instatiation of the user class

---
+++ classes/user.py
@@ -0,0 +1,12 @@
+import pyperclip
+
+class User:
+    '''
+    Class that instantiates the user and their details.
+    '''
+
+    account_list =[]
+    def __init__(self, user_name, email, password):
+        '''
+        init method for the user details 
+        '''
