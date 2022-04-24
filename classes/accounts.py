68613945d81044d0d04612a07510ee5bde41c161
Ngina <Nginagitau11@outlook.com>
2022-04-24 19:32:56 +0300

    update: adds the instatiation of the accounts class

---
+++ classes/accounts.py
@@ -0,0 +1,13 @@
+import pyperclip
+
+class Accounts:
+    '''
+    Class that instantiates the accounts and their details.
+    '''
+
+    account_list =[]
+    def __init__(self, account_name, user_name, login_email, password):
+        '''
+        init method for the account list details
+        '''
+
